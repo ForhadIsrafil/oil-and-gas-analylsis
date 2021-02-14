ad24b87cfb05868dedcd7514d02d24a52a171bee
forhadisrafil <forhadtuhin5088@gmail.com>
2021-02-15 03:19:39 +0600

    new design

--- app1.py
+++ app1.py
@@ -1,47 +1,27 @@
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table
-from datetime import datetime as dt
-from datetime import date, timedelta
-import pandas as pd
+from dash.dependencies import Output, Input
 import plotly.express as px
-from dash.dependencies import Output, Input, MATCH, State
-import dash_auth
-import plotly.graph_objs as go
-# from plotly.subplots import make_subplots
-from plotly.subplots import make_subplots
 import dash_bootstrap_components as dbc
+import pandas as pd
+# import pandas_datareader.data as web
+import datetime
 
-# see https://community.plot.ly/t/nolayoutexception-on-deployment-of-multi-page-dash-app-example-code/12463/2?u=dcomfort
-# from app import server
-from app import app
-from flask import request
 from data.og_data import get_csv_data
 
-# see https://dash.plot.ly/external-resources to alter header, footer and favicon
-# app.index_string = '''
-# <!DOCTYPE html>
-# <html>
-#     <head>
-#         {%metas%}
-#         <title>New York Oil and Gas Report</title>
-#         {%favicon%}
-#         {%css%}
-#     </head>
-#     <body>
-#         {%app_entry%}
-#         <footer>
-#             {%config%}
-#             {%scripts%}
-#         </footer>
-#         <div>New York Oil and Gas Report</div>
-#     </body>
-# </html>
-# '''
-from plotly.validators.pie import domain
+
+# https://www.bootstrapcdn.com/bootswatch/
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}]
+                )
+
+# Layout section: Bootstrap (https://hackerthemes.com/bootstrap-cheatsheet/)
+# ************************************************************************
+
 
 app.layout = dbc.Container([
-    dcc.Location(id='url', refresh=False),
     ######-------- 1st row start ---------######
     dbc.Row([
         dbc.Col(children=[html.Img(src='assets/dash-logo.png', height=100, width=200)],
@@ -51,7 +31,7 @@ app.layout = dbc.Container([
                           html.H6("Production Overview",
                                   )],
                 className='text-center',
-                width={'size': 4}),
+                width={'size': 4})
 
     ],
         style={'font-size': 10},
@@ -171,138 +151,52 @@ app.layout = dbc.Container([
 
         dbc.Col(dbc.Row(
             [
-                html.Div(children=[html.H4('3225', id='no_wells', ), html.H6('No. of Wells')],
+                html.Div(children=[html.H4('3225'), html.H6('No. of Wells')],
                          style={'box-shadow': '2px 2px 2px lightgrey'},
                          className='card-body p-2 m-2 text-center border rounded',
                          ),
-                html.Div(children=[html.H4('425M mcf', id='no_gas', ), html.H6('Gas')],
+                html.Div(children=[html.H4('425M mcf'), html.H6('Gas')],
                          style={'box-shadow': '2px 2px 2px lightgrey'},
 
                          className='card-body p-2 m-2 text-center border rounded'),
-                html.Div(children=[html.H4('2M bbl', id='no_oil', ), html.H6('Oil')],
+                html.Div(children=[html.H4('2M bbl'), html.H6('Oil')],
                          style={'box-shadow': '2px 2px 2px lightgrey'},
 
                          className='card-body p-2 m-2 text-center border rounded'),
-                html.Div(children=[html.H4('3M bbl', id='no_water', ), html.H6('Water')],
+                html.Div(children=[html.H4('3M bbl'), html.H6('Water')],
                          style={'box-shadow': '2px 2px 2px lightgrey'},
-                         className='card-body p-2 m-2 text-center border rounded'),
-                # bar chart start
-                # dbc.Col(children=[
-                dcc.Graph(id='og_bar_chart', figure={}, )
-                # ]),
-                # bar chart end
 
-            ], ),
+                         className='card-body p-2 m-2 text-center border rounded')
+
+            ],
+
         ),
-    ], justify='start'),
 
-    # pie chart start
-    dbc.Row(children=[
-        dbc.Col(children=[
-            # html.H4('Production Summery: 2006 to 2019', className='text-center text-white', ),
-            dcc.Graph(id='og_pie_chart1', figure={}, style={'display': 'inline-block'}),
-            dcc.Graph(id='og_pie_chart2', figure={}, style={'display': 'inline-block'}),
-        ], style={'width': 480}),
-        dbc.Col(children=[
-            dcc.Graph(id='og_line_chart', figure={}, style={'height': 446, 'width': 480})
-        ], )
-    ],
-        # style={'background-color': '#212529'},
-    ),
-    # pie chart end
+        ),
+        dbc.Row(children = [
+            dcc.Graph(id='first_bar', figure={})]
+
+        )
+
+    ], justify='start'),
 ], )
 
 
-# app.layout = html.Div([
-#     dcc.Location(id='url', refresh=False),
-#     # html.Div(dash_auth.create_logout_button(), className='two columns', style={'marginTop': 30}),
-#     # html.Div(id='page-content'),
-#
-#     # bar chart start
-#     dbc.Col(children=[
-#         dcc.Graph(id='og_bar_chart', figure={})
-#     ]),
-#     # bar chart end
-#
-#     # pie chart start
-#     dbc.Row(children=[
-#         dbc.Col(children=[
-#             html.H4('Production Summery: 2006 to 2019', className='text-center text-white', ),
-#             dcc.Graph(id='og_pie_chart1', figure={}, style={'display': 'inline-block'}),
-#             dcc.Graph(id='og_pie_chart2', figure={}, style={'display': 'inline-block'}),
-#         ], ),
-#         dbc.Col(children=[
-#             dcc.Graph(id='og_line_chart', figure={}, )
-#         ], )
-#     ],
-#         # style={'background-color': '#212529'},
-#     ),
-#     # pie chart end
-#
-# ])
-
-
-# pie chart start
-
-
-# Update page
-# # # # # # # # #
-
-@app.callback(
-    [Output('og_bar_chart', 'figure'), Output('og_pie_chart1', 'figure'), Output('og_pie_chart2', 'figure'),
-     Output('og_line_chart', 'figure'), Output('no_wells', 'children'), Output('no_gas', 'children'),
-     Output('no_oil', 'children'),
-     Output('no_water', 'children'), ],
-    Input('url', 'pathname'))
-def display_page(pathname):
-    if pathname == '/home/':
-        data = get_csv_data().copy()
-        # data = data.groupby('Wl_Status', as_index=False)
-        gas = data['GasProd'].sum()
-        oil = data['OilProd'].sum()
-        water = data['WaterProd'].sum()
-        no_wells = gas + water + oil
-        print(no_wells)
-        # print(data.head(10))
-        # bar figure start
-        bar_group = data[data['Wl_Status'] == 'AC'].groupby('Year', as_index=False).sum()
-        bar_fig = px.bar(bar_group, x='Year', y='Completion', text='Completion',
-                         color_discrete_sequence=px.colors.sequential.Aggrnyl, height=446)
-        bar_fig.update_layout(
-            title={'text': 'Completed Wells/Year', 'y': 0.9, 'x': 0.5, 'xanchor': 'center', 'yanchor': 'top'},
-            yaxis_title='')
-        # bar figure end
-
-        # pie figure start
-        group_data = data.groupby(['Year', 'Completion'], as_index=False).sum()
-        pie_fig1 = px.pie(data_frame=group_data, names=['GasProd', 'WaterProd', 'OilProd'],
-                          # labels=['GasProd', 'WaterProd', 'OilProd'],
-                          hole=0.5,
-                          # template='plotly_dark',  # presentation, plotly_dark
-                          # title='Production Summery: 2006 to 2019',
-                          width=240,
-                          color_discrete_sequence=px.colors.sequential.Aggrnyl,
-                          )
-        pie_fig1.update_layout(
-            legend=dict(orientation="h", yanchor="bottom", y=-0.30, xanchor="left", x=0.01, ), )
-        # pie figure end
-
-        # line figure start
-        line_data = data.groupby(['Year'], as_index=False).mean()
-        line_fig = px.line(line_data, x='Year', y=['GasProd', 'WaterProd', 'OilProd'],
-                           title='Aggregate: Oil Development', hover_name='Year',
-                           labels={"y": "",
-                                   "WaterProd": "Water Produced (bbl)",
-                                   "OilProd": "Oil Produced (bbl)"}, )
-        line_fig.update_layout(title={'y': 0.9, 'x': 0.5, 'xanchor': 'center', 'yanchor': 'top'}, yaxis_title='',
-                               legend=dict(orientation="h", yanchor="bottom", y=-0.30, xanchor="left", x=0.01,
-                                           title=''),
-                               margin=dict(t=2, l=2, b=2, r=2))
-        # line figure end
-
-        return bar_fig, pie_fig1, pie_fig1, line_fig, no_wells, gas, oil, water
-    else:
-        return '404'
+@app.callback(Output('first_bar', 'figure'),
+              Input('bar_input', 'value'))
+def all_graph(first_bar_input):
+    data = get_csv_data()
+    # line figure start
+    line_data = data.groupby(['Year'], as_index=False).sum()
+    line_fig = px.line(line_data, x='Year', y=['GasProd', 'WaterProd', 'OilProd'],
+                       title='Aggregate: Oil Development', hover_name='Year',
+                       labels={"y": "",
+                               "WaterProd": "Water Produced (bbl)",
+                               "OilProd": "Oil Produced (bbl)"}, )
+    line_fig.update_layout(title={'y': 0.9, 'x': 0.5, 'xanchor': 'center', 'yanchor': 'top'},
+                           legend=dict(orientation="h", yanchor="bottom", y=-0.30, xanchor="left", x=0.01),
+                           margin=dict(t=2, l=2, b=2, r=2))
+    return line_fig
 
 
 if __name__ == '__main__':
